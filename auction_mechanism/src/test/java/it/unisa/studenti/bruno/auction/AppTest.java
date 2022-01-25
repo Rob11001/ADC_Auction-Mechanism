51a065922c2b12c75dbac542f355d0670b4fc996
Roberto Bruno <45689820+Rob11001@users.noreply.github.com>
2022-01-25 12:08:23 +0100

    Maven Initialization

---
+++ auction_mechanism/src/test/java/it/unisa/studenti/bruno/auction/AppTest.java
@@ -0,0 +1,19 @@
+package it.unisa.studenti.bruno.auction;
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+       
+    }
+}
